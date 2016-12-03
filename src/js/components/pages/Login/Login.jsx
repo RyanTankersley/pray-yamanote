012c06b5043e860228e246ca49b2f31893790e18
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-03 19:44:39 +0900

    Added buttons to login page

--- src/js/components/pages/Login/Login.jsx
+++ src/js/components/pages/Login/Login.jsx
@@ -5,9 +5,20 @@ import PageHeader from '../../shared/PageHeader.jsx';
 
 class Login extends React.Component{
   render() {
+    const buttonStyle = {
+      width: '100px'
+    };
+
     return(
       <div>
         <PageHeader />
+        <div style={{'padding': '10px', 'textAlign': 'center'}}>
+          <h3>Please choose a service to login through</h3>
+          <div className="btn-group-vertical" role="group">
+            <button className='btn btn-primary'>Facebook</button>
+            <button className='btn btn-primary'>Google</button>
+          </div>
+        </div>
       </div>
     );
   }
