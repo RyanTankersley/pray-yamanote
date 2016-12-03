2321328bc1da1ce675ca1d6d8acc0a8082eff9bd
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-03 20:59:43 +0900

    Added routes for creating users

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const moment = require('moment');
 const config = require('./lib/config');
 const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy;
+require('./lib/js/database/Connection.js');
+const usersApiItem = require('./lib/js/database/Users.js');
+const usersApi = new usersApiItem();
 
 const app = express();
 
@@ -15,35 +18,54 @@ app.use(bodyParser.json());
 const router = express.Router();
 
 router.get('/stations', function(req, res) {
-    const file = __dirname + '/lib/data/english/stations.json';
-    res.sendFile(file);
+  const file = __dirname + '/lib/data/english/stations.json';
+  res.sendFile(file);
 });
 
 router.get('/strings', function(req, res) {
-    const file = __dirname + '/lib/data/english/strings.json';
-    res.sendFile(file);
+  const file = __dirname + '/lib/data/english/strings.json';
+  res.sendFile(file);
 });
 
+router.post('/user/:email/:fname/:lname', function(req, res) {
+  let email = req.params.email;
+  let fname = req.params.fname;
+  let lname = req.params.lname;
+  usersApi.Create(email, fname, lname, (response) => {
+      res.send(response);
+  });
+});
 
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://www.example.com/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  }
-));
+router.put('/user/:email/:fname/:lname', function(req, res) {
+  let email = req.params.email;
+  let fname = req.params.fname;
+  let lname = req.params.lname;
+  usersApi.Update(email, fname, lname, (response) => {
+      res.send(response);
+  });
+});
+
+router.get('/user/:email/', function(req, res) {
+  let email = req.params.email;
+  usersApi.Get(email, (response) => {
+      res.send(response);
+  });
+});
+
+router.delete('/user/:email/', function(req, res) {
+  let email = req.params.email;
+  usersApi.Delete(email, (response) => {
+      res.send(response);
+  });
+});
+
 app.use(express.static('public'));
 
 app.use('/api', router);
 
 app.get('/*', (req, res) => {
-    const file = __dirname + '/index.html';
-    res.sendFile(file);
+  const file = __dirname + '/index.html';
+  res.sendFile(file);
 });
 
 app.listen(config.port);
