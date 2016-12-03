2ba59e772f97f8d40d7703f93289d01b77ad4a22
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-03 19:25:09 +0900

    Added login page with route

---
+++ src/js/components/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import PageHeader from '../../shared/PageHeader.jsx';
+
+class Login extends React.Component{
+  render() {
+    return(
+      <div>
+        <PageHeader />
+      </div>
+    );
+  }
+};
+
+module.exports = Login;
--- src/js/routes.js
+++ src/js/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Router, Route, browserHistory, DefaultRoute, NotFoundRoute, Redirect } from 'react-router'
 const HomePage = require('./components/pages/Home.jsx')
 const PrayerStation = require('./components/pages/PrayerStation/PrayerStation.jsx')
+const Login = require('./components/pages/Login/Login.jsx')
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={HomePage} />
     <Route path="/station/:station/:isGoingClockwise" component={PrayerStation} />
+    <Route path="/login" component={Login} />
   </Router>
 );
 
