9d507051a5eb84ae81ad1f2cb9ec38a2357c1412
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-13 23:31:27 +0900

    Created walk creator

--- src/js/components/pages/WalkCreator/WalkCreator.jsx
+++ src/js/components/pages/WalkCreator/WalkCreator.jsx
@@ -5,11 +5,17 @@ import React from 'react';
 import PageHeader from '../../shared/PageHeader.jsx';
 import AuthRequiredComponent from '../../shared/AuthRequiredComponent.jsx';
 import AccountApi from '../../../api/Account.js';
+import StationApi from '../../../api/Station.js';
 import StationCreator from './StationCreator.jsx';
+import { browserHistory } from 'react-router';
+import { walkSchema } from '../../../../../lib/js/database/PrayerWalk.js';
 
 class WalkCreator extends AuthRequiredComponent{
   constructor() {
     super();
+    this.state.name = "";
+    this.state.image = "";
+    this.state.imageError = false;
   }
 
   render() {
@@ -21,23 +27,75 @@ class WalkCreator extends AuthRequiredComponent{
     }
   }
 
-  renderMain() {
-    const labelStyle = {
-      paddingRight: '5px'
+  onNameChange(text) {
+    this.state.name = text;
+    this.setState(this.state);
+  }
+
+  onImageChange(text) {
+    this.state.imageError = false;
+    this.state.image = text;
+    this.setState(this.state);
+  }
+
+  onCancel() {
+    browserHistory.push("/account");
+  }
+
+  onFinish() {
+    var account = AccountApi.getAccount();
+    if(this.state.name !== "" && this.state.image !== "" && this.state.imageError !== true) {
+      console.log(account);
+      StationApi.createWalk(this.state.name, account.email, this.state.image);
+      browserHistory.push("/account");
     }
+  }
+
+  testImage(text) {
+    const image = new Image();
+    image.addEventListener("error", (e) => {
+      console.log('rawr');
+      this.state.imageError = true;
+      this.setState(this.state);
+    });;
+
+    image.src = text;
+  }
+
+  renderMain() {
+    const inputStyle = {
+      maxWidth: '500px'
+    };
+
+    let image = null;
+    if(!this.state.imageError && this.state.image !== "") {
+      this.testImage(this.state.image);
+      image = (<img src={this.state.image} style={{'maxWidth': '500px', 'width': '100%'}}/>);
+    }
+
+    if(this.state.imageError) {
+      image = (<p className='text-danger'>Invalid image link</p>);
+    }
+
     return (
       <div>
         <PageHeader />
         <div style={{'padding': '10px', 'paddingTop': '0'}}>
-          <h3 style={{'padding-left': '5px'}}>Create New Prayer Walk</h3>
-          <div className='row' style={{'padding': '5px'}}>
-            <label style={labelStyle}>Station Name</label><input id='name' type='text' />
-          </div>
-          <StationCreator stations={[]} />
-
-          <div className="row" style={{'padding': '5px'}}>
-            <button type="button" className="btn btn-danger" style={{'float': 'left', 'marginRight': '5px'}} onClick={(e) => this.props.onMove(true)}>Cancel</button>
-            <button type="button" className="btn btn-success" onClick={(e) => this.onStartOver()}>Finish</button>
+          <h3>Create New Prayer Walk</h3>
+          <form>
+            <div className="form-group">
+              <label>Prayer Walk Name</label>
+              <input style={inputStyle} type="text" className="form-control" id="name" placeholder="Ex. Around the City" value={this.state.name} onChange={(e) => this.onNameChange(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label>Link to Image</label>
+              <input style={inputStyle} type="text" className="form-control" id="name" placeholder="Ex. http://www.photobucket.com/1234/aroundthecity.jpg" value={this.state.image} onChange={(e) => this.onImageChange(e.target.value)} />
+            </div>
+          </form>
+          {image}
+          <div className="row">
+            <button type="button" className="btn btn-danger" style={{'float': 'left', 'marginRight': '5px'}} onClick={(e) => this.onCancel()}>Cancel</button>
+            <button type="button" className="btn btn-success" onClick={(e) => this.onFinish()}>Finish</button>
           </div>
         </div>
       </div>
