b2d4326e003112545f0f938c7ba06d55e3b76cd3
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-13 23:31:15 +0900

    Added create walk method to station api

--- src/js/api/Station.js
+++ src/js/api/Station.js
@@ -17,6 +17,21 @@ class Station {
         cb({err: true, response: 'An error occurred while attempting to retrieve stations.'});
       });
   };
+  
+  static createWalk(name, owner, image, cb) {
+
+    if(stations !== null)
+      cb(stations);
+
+    $.post(`/api/walk/${name}/${owner}/${image}/`)
+      .done((response) => {
+        stations = {err: false, response: response};
+        cb(stations);
+      })
+      .fail((response) => {
+        cb({err: true, response: 'An error occurred while attempting to retrieve stations.'});
+      });
+  };
 };
 
 module.exports = Station;
