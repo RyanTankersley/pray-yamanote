26b851cd7305419ca41c596564e15a0b35bff485
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2016-12-17 10:04:42 +0900

    Finished client side validation on walk creator

--- src/js/components/pages/WalkCreator/WalkCreator.jsx
+++ src/js/components/pages/WalkCreator/WalkCreator.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import mongoose from 'mongoose';
 
 import PageHeader from '../../shared/PageHeader.jsx';
+import ValidationInput from '../../shared/ValidationInput.jsx';
 import AuthRequiredComponent from '../../shared/AuthRequiredComponent.jsx';
 import AccountApi from '../../../api/Account.js';
 import StationApi from '../../../api/Station.js';
@@ -18,6 +19,8 @@ class WalkCreator extends AuthRequiredComponent{
     this.state.name = "";
     this.state.image = "";
     this.state.imageError = false;
+    this.state.nameHasHadValue = false;
+    this.state.imageHasHadValue = false;
   }
 
   render() {
@@ -30,11 +33,13 @@ class WalkCreator extends AuthRequiredComponent{
   }
 
   onNameChange(text) {
+    this.state.nameHasHadValue = true;
     this.state.name = text;
     this.setState(this.state);
   }
 
-  onImageChange(text) {
+  onImageChange(text, me) {
+    this.state.imageHasHadValue = true;
     this.state.imageError = false;
     this.state.image = text;
     this.setState(this.state);
@@ -45,16 +50,25 @@ class WalkCreator extends AuthRequiredComponent{
   }
 
   onFinish() {
-    var account = AccountApi.getAccount();
-    if(this.state.name !== "" && this.state.image !== "" && this.state.imageError !== true && this.validate(this.state.name, account.email, this.state.image)) {
-      console.log(account);
+    this.state.nameHasHadValue = true;
+    this.state.imageHasHadValue = true;
+    if(this.isValid()) {
+      const account = AccountApi.getAccount();
       StationApi.createWalk(this.state.name, account.email, this.state.image);
       browserHistory.push("/account");
     }
+    
+    this.setState(this.state);
   }
 
-  validate(name, owner, image) {
-    let doc = new mongoose.Document({name: name, owner: owner, image: image});
+  getMongoDocFromState(thismethisowner, image) {
+    var account = AccountApi.getAccount();
+    return this.getMongoDoc(this.state.name, account.email, this.state.image);
+  }
+
+  getMongoDoc(name, owner, image) {
+    let doc = new mongoose.Document({name: name, owner: owner, image: image}, walkSchema);
+    return doc;
   }
 
   testImage(text) {
@@ -68,40 +82,86 @@ class WalkCreator extends AuthRequiredComponent{
     image.src = text;
   }
 
-  renderMain() {
+  getFormInput(labelName, hasError, placeholder, value, onChange, errorText) {
+    const formGroupClass = 'form-group' + (hasError ? ' has-error' : '');
     const inputStyle = {
       maxWidth: '500px'
     };
 
+    let error = null;
+    if(hasError) {
+      error = (<p className='text-danger'>{errorText}</p>)
+    }
+    return (
+      <div className={formGroupClass}>
+        <label>{labelName}</label>
+        <input style={inputStyle} type="text" className="form-control" id={labelName} placeholder={placeholder} value={value} onChange={(e) => onChange(e.target.value, this)} />
+        {error}
+      </div>
+    );
+  }
+
+  nameHasError() {
+    if(!this.state.nameHasHadValue) return false;
+
+    let errors = this.getMongoDocFromState().validateSync();
+    return errors !== undefined && errors.errors['name'] !== undefined ? true : false;
+  }
+
+  imageHasError() {
+    if(!this.state.imageHasHadValue) return false;
+    if(this.state.imageError) return true;
+
+    let errors = this.getMongoDocFromState().validateSync();
+    return errors !== undefined && errors.errors['image'] !== undefined ? true : false;
+  }
+
+  isValid() {
+    return !this.imageHasError() && !this.nameHasError();
+  }
+
+  getImage() {
     let image = null;
+
     if(!this.state.imageError && this.state.image !== "") {
       this.testImage(this.state.image);
       image = (<img src={this.state.image} style={{'maxWidth': '500px', 'width': '100%'}}/>);
     }
 
-    if(this.state.imageError) {
-      image = (<p className='text-danger'>Invalid image link</p>);
-    }
+    return image;
+  }
 
+  getFinishButton(enabled) {
+    const type = 'button';
+    const className = 'btn btn-success';
+    const text = 'Finish';
+    const handler = (e) => this.onFinish();
+    
+    let finishButton = null;
+    if(enabled)
+      finishButton = (<button type={type} className={className} onClick={handler}>{text}</button>);
+    else
+      finishButton = (<button type={type} className={className} onClick={handler} disabled>{text}</button>);
+
+    return finishButton;
+  }
+
+  renderMain() {
     return (
       <div>
         <PageHeader />
         <div style={{'padding': '10px', 'paddingTop': '0'}}>
           <h3>Create New Prayer Walk</h3>
           <form>
-            <div className="form-group">
-              <label>Prayer Walk Name</label>
-              <input style={inputStyle} type="text" className="form-control" id="name" placeholder="Ex. Around the City" value={this.state.name} onChange={(e) => this.onNameChange(e.target.value)} />
-            </div>
-            <div className="form-group">
-              <label>Link to Image</label>
-              <input style={inputStyle} type="text" className="form-control" id="name" placeholder="Ex. http://www.photobucket.com/1234/aroundthecity.jpg" value={this.state.image} onChange={(e) => this.onImageChange(e.target.value)} />
-            </div>
+            <ValidationInput labelName='Prayer Walk Name' hasError={this.nameHasError()} placeholder='Ex. Around the City' 
+                             value={this.state.name} onChange={(e) => this.onNameChange(e.target.value)} errorText='Name cannot be empty'/>
+            <ValidationInput labelName='Link to Image' hasError={this.imageHasError()} placeholder='Ex. http://www.photobucket.com/1234/aroundthecity.jpg' 
+                             value={this.state.image} onChange={(e) => this.onImageChange(e.target.value)} errorText='Image link invalid.'/>
           </form>
-          {image}
+          {this.getImage()}
           <div className="row">
             <button type="button" className="btn btn-danger" style={{'float': 'left', 'marginRight': '5px'}} onClick={(e) => this.onCancel()}>Cancel</button>
-            <button type="button" className="btn btn-success" onClick={(e) => this.onFinish()}>Finish</button>
+            {this.getFinishButton(this.isValid())}
           </div>
         </div>
       </div>
